05d6a3eed5ed2c9ce95c8550706cf1add162b4a9
Robben <robbenwadlay@163.com>
2024-09-12 14:38:00 +0800

    update BLE scanning page

--- src/pages/discovery/index.vue
+++ src/pages/discovery/index.vue
@@ -17,6 +17,8 @@ import GlobalProvider from '@/components/GlobalProvider.vue';
 import BleManager from '@/modules/BleManager';
 import { type BleDeviceInfo } from '@/modules/BleManager/BleDeviceInfo';
 import { type BleManagerError } from '@/modules/BleManager/BleManagerError';
+import Permission from '@/modules/Permission';
+import PermissionTypeEnum from '@/modules/Permission/enums/PermissionTypeEnum';
 import System from '@/modules/System';
 import SystemURLEnum from '@/modules/System/enums/SystemURLEnum';
 import { Timeout } from '@/modules/Timer/Timeout';
@@ -27,7 +29,7 @@ import { onUnmounted, Ref, ref } from 'vue';
 
 const globalProvider = ref<InstanceType<typeof GlobalProvider> | null>(null);
 const bleManager = BleManager.getInstance(['0000FF00-0000-1000-8000-00805F9B34FB']);
-const stopScanTimer = new Timeout(2500, async () => await bleManager.stopScan());
+const stopScanTimer = new Timeout(2500, stopScan);
 
 const discoveredDevices: Ref<BleDeviceInfo[]> = ref([]);
 
@@ -40,7 +42,7 @@ onUnmounted(async () => {
   if (!stopScanTimer.excuted) {
     console.warn('onUnmounted-', stopScanTimer.excuted);
     stopScanTimer.clear();
-    await bleManager.stopScan();
+    stopScan();
   }
 });
 /**
@@ -58,48 +60,24 @@ async function init() {
   } catch (e) {
     console.error(e);
     const { code } = e as BleManagerError;
-    if (/*iOS中未授权*/ System.isIOS && code == 10000) {
-      const { confirm, cancel } = await uni.showModal({ title: '蓝牙位置权限未授权', content: '是否进行授权?' });
-      if (confirm) uni.openAppAuthorizeSetting();
-      if (cancel) uni.navigateBack();
+    if (/*iOS未授权蓝牙权限*/ System.isIOS && code == 10000) {
+      await showAuthRequestModal();
     }
     if (/*蓝牙适配器不可用*/ code == 10001) {
       const { confirm, cancel } = await uni.showModal({ title: '蓝牙适配器不可用', content: '是否打开蓝牙适配器开关?' });
       if (/*打开系统蓝牙设置*/ confirm) System.openSystemSetting(SystemURLEnum.BLUETOOTH);
       if (cancel) uni.navigateBack();
     }
+    return;
   }
   // 蓝牙权限检测
-  if (System.isAndroid) {
-    const Build = plus.android.importClass('android.os.Build');
-    let permissions = ['android.permission.ACCESS_FINE_LOCATION'];
-
-    // @ts-ignore
-    if (Build.VERSION.SDK_INT >= 31) permissions.push('android.permission.BLUETOOTH_SCAN');
-    plus.android.requestPermissions(
-      permissions,
-      async ({ deniedAlways, deniedPresent, granted }) => {
-        // 权限被永久拒绝 - 弹框提示需要权限 - 引导用户打开设置页面开启权限
-        if (deniedAlways.length > 0) {
-          const { confirm, cancel } = await uni.showModal({ title: '蓝牙位置权限未授权', content: '是否进行授权?' });
-          if (confirm) uni.openAppAuthorizeSetting();
-          if (cancel) uni.navigateBack();
-        }
-        // 权限被临时拒绝 - 再次申请
-        if (deniedPresent.length > 0) {
-          plus.android.requestPermissions(
-            deniedPresent,
-            ({ granted }) => granted.length == deniedPresent.length && startScan(),
-            (e) => console.error('Request Permissions error:', e)
-          );
-        }
-
-        if (granted.length == permissions.length) startScan();
-      },
-      (e) => console.error('Request Permissions error:', e)
-    );
-  } else if (System.isIOS) {
-    startScan();
+  try {
+    const authorized = await Permission.requestPermissions(PermissionTypeEnum.BLUETOOTH);
+    if (authorized) startScan();
+    else await showAuthRequestModal();
+  } catch (e) {
+    console.error(e);
+    return;
   }
 }
 
@@ -112,10 +90,24 @@ async function startScan() {
     console.error(e);
   }
 }
-
+async function stopScan() {
+  console.warn('停止扫描');
+  try {
+    await bleManager.stopScan();
+  } catch (e) {
+    console.error(e);
+  }
+}
 function connect(selectedDeviceInfo: BleDeviceInfo) {
   useSelectedDeviceInfoStore().$patch({ selectedDeviceInfo });
   uni.switchTab({ url: '/pages/device/index' });
+}
+
+/** 引导用户开启蓝牙权限 */
+async function showAuthRequestModal() {
+  const { confirm, cancel } = await uni.showModal({ title: '蓝牙位置权限未授权', content: '是否进行授权?' });
+  if (confirm) uni.openAppAuthorizeSetting();
+  if (cancel) uni.navigateBack();
 }
 </script>
 <style scoped lang="scss"></style>
